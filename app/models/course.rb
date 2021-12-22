fa2583b780d58ce78ce598028cf2b364f063b25d
mpalak <mateusz.palak@hotmail.com>
2021-12-22 19:35:38 +0000

    add rich_text_area

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
   validates :title,  presence: true
-  validates :description, presence: true, length: { :minimum => 5 }
+  validates :description, presence: true
+  has_rich_text :description
 end
