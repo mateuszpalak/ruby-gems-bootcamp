d3167432557d4fee1dd23d0ca2fb761c0ee52c98
mpalak <mateusz.palak@hotmail.com>
2021-12-22 18:35:56 +0000

    gem simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#landing_page'
-#  get 'static_pages/landing_page'
-#  get 'static_pages/privacy_policy'
+  get 'home/index'
+  root 'home#index'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
